7c73514a6cde1f7c3502813662d9f589274e270f
József Bicsák <bicsi@komarom.net>
2021-10-21 12:09:54 +0200

    belépés oop

--- belepes.php
+++ belepes.php
@@ -3,24 +3,26 @@
 session_start();
 
 require 'db.inc.php';
-
+require 'model/Ulesrend.php';
 // form feldolgozása
-
+$tanulo = new Ulesrend;
 
 if(isset($_POST['user']) and isset($_POST['pw'])) {
 	$loginError = '';
 	if(strlen($_POST['user']) == 0) $loginError .= "Nem írtál be felhasználónevet<br>";
 	if(strlen($_POST['pw']) == 0) $loginError .= "Nem írtál be jelszót<br>";
 	if($loginError == '') {
-		$sql = "SELECT id, nev, jelszo FROM ulesrend WHERE felhasznalonev = '".$_POST['user']."' ";
+		$sql = "SELECT id FROM ulesrend WHERE felhasznalonev = '".$_POST['user']."' ";
 
 		if(!$result = $conn->query($sql)) echo $conn->error;
 
 		if ($result->num_rows > 0) {
+			
 			if($row = $result->fetch_assoc()) {
-				if(md5($_POST['pw']) == $row['jelszo']) {
+				$tanulo->set_user($row['id'], $conn);
+				if(md5($_POST['pw']) == $tanulo->get_jelszo()) {
 					$_SESSION["id"] = $row['id'];
-					$_SESSION["nev"] = $row['nev'];
+					$_SESSION["nev"] = $tanulo->get_nev();
                     header('Location: ulesrend.php');
                     exit();
 				}
