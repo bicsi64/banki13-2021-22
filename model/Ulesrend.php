f2db7b5108e6600708262d835e83adcd8a61c3ca
József Bicsák <bicsi@komarom.net>
2021-11-04 08:34:41 +0100

    model

--- model/Ulesrend.php
+++ model/Ulesrend.php
@@ -62,7 +62,7 @@ class Ulesrend {
         if($result = $conn->query($sql)) {
             if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()) {
-                    
+                    $lista[] = $row['id'];
                 }
             }
         }
