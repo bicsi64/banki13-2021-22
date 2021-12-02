afdcdc4def54a704bbe8bc55bf726ded2257c64f
József Bicsák <bicsi@komarom.net>
2021-12-02 12:23:38 +0100

    file upload

--- upload.php
+++ upload.php
@@ -1,62 +1,54 @@
 <?php
+  $i = 0;
+  $errors = array();
 
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+if(isset($_FILES["fileToUpload"])) {
+  echo '<pre>';
+  print_r($_FILES["fileToUpload"]);
+  echo '</pre>';
+  $target_dir = "uploads/";
+  $allowed_filetypes = array('image/png', 'image/jpg','image/jpeg');
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
+  foreach($_FILES["fileToUpload"]["name"] as $key => $name) {
+     $target_file = $target_dir . basename($name);
+
+    if ($_FILES["fileToUpload"]["size"][$key] > 102400) {
+      $errors[$key][] = "A $name túl nagy méretű, 100KB-nál nem lehet nagyobb";
+    }
+    elseif ($_FILES["fileToUpload"]["size"][$key] < 1024) {
+      $errors[$key][] = "A $name túl kis méretű, 1KB-nál nem lehet kisebb";
+    }
+
+    if (!in_array($_FILES["fileToUpload"]["type"][$key], $allowed_filetypes) ) {
+      $errors[$key][] = "A $name file nem jpg vagy png.";
+    }
+
+    if(!isset($errors[$key])) {
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
+        $i++;
+      }      
+    }
   }
 }
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
 
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
+if($i > 0) echo "$i fájl feltöltve";
+if($errors) {
+  foreach ($errors as $error) {
+    foreach ($error as $errorMsg) {
+      echo "$errorMsg <br>";
+    }
   }
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<body>
-
 <form action="upload.php" method="post" enctype="multipart/form-data">
   Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
   <input type="submit" value="Upload Image" name="submit">
 </form>
 
