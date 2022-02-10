87bdf8f6e917b8c11185a4e8cddee920df38471b
József Bicsák <bicsi@komarom.net>
2022-02-10 12:27:19 +0100

    prepared

--- ajax.php
+++ ajax.php
@@ -6,8 +6,29 @@ require 'includes/db.inc.php';
 
 if(isset($_REQUEST['keres'])) {
    
-   
-    // echo $_REQUEST['keres'];
+    $sql = 'SELECT nev FROM ulesrend WHERE nev LIKE ?';
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param('s', $like); 
+    
+    $like = $_REQUEST['keres'].'%';
+    
+    $stmt->execute();
+
+    if($result = $stmt->get_result()) {
+
+        if ($result->num_rows > 0) {
+            
+            while($row = $result->fetch_assoc()) {
+                echo $row['nev']."<br>";
+            }
+        }
+        else echo 'Nincs ilyen név';        
+    } 
+
+
+    // 
 }
 
 
